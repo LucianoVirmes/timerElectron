2645bea0e38e1b692d13a2004e8cb1c7f38ab856
LucianoVirmes <luciano.virmes18@gmail.com>
2019-04-17 00:17:23 -0300

    alterações feitas no css e html, adicionado momentjs para trabalhar com tempo e implementado um contador de segundos ao cronometro

--- alura-electron/app/js/renderer.js
+++ alura-electron/app/js/renderer.js
@@ -1,7 +1,17 @@
 const {ipcRenderer} = require('electron');
+const timer = require('./timer');
 
 let linkSobre = document.querySelector('#link-sobre');
-
+let botaoPlay = document.querySelector('.botao-play');
+let tempo = document.querySelector('.tempo');
 linkSobre.addEventListener('click', () =>{
     ipcRenderer.send("abrir-janela-sobre");
+})
+
+let imgs = ['img/play-button.svg', 'img/stop-button.svg'];
+
+botaoPlay.addEventListener('click', () =>{
+    timer.iniciar(tempo);
+    imgs = imgs.reverse();
+    botaoPlay.src = imgs[0];
 })
